e18a0a71bc82da38960004a92daaae66b98e0cf1
radwa <rmamdouh333@gmail.com>
2024-11-23 21:27:14 +0200

    Arabic is AR in responsive

--- src/components/LanguageBtn/LanguageBtn.js
+++ src/components/LanguageBtn/LanguageBtn.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { Button } from "primereact/button";
 import { useTranslation, withTranslation } from "react-i18next";
 import { globe } from "icons";
@@ -10,10 +11,28 @@ const LanguageBtn = ({ ...props }) => {
 	const currLang = i18n.language;
 	document.body.dir = i18n.dir();
 
+	const [smallSize, setSmallSize] = useState(false);
+
 	const changeLanguage = (lng) => {
 		i18n.changeLanguage(lng);
 		document.body.dir = i18n.dir();
 	};
+
+	useEffect(() => {
+		const handleResize = () => {
+			setSmallSize(window.innerWidth < 992);
+		};
+
+		// Add event listener for resize
+		window.addEventListener("resize", handleResize);
+		// window.addEventListener("load", handleResize);
+
+		// Remove event listener on component unmount
+		return () => {
+			window.removeEventListener("resize", handleResize);
+			// window.removeEventListener("load", handleResize);
+		};
+	}, [setSmallSize]);
 
 	return (
 		<Button
@@ -22,7 +41,7 @@ const LanguageBtn = ({ ...props }) => {
 			className={`${styles.lang_btn} ${props.classes}`}
 			onClick={() => changeLanguage(currLang === "ar" ? "en" : "ar")}>
 			{globe}
-			<span>{currLang === "ar" ? "EN" : "Arabic"}</span>
+			<span>{currLang === "ar" ? "EN" : smallSize ? "AR" : "Arabic"}</span>
 		</Button>
 	);
 };
