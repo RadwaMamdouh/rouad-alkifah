69b81e7a351cee29bdfd3e90888025d429da6cd3
radwa <rmamdouh333@gmail.com>
2024-12-31 00:40:19 +0200

    enhance contact

--- src/pages/ContactUs/ContactUs.js
+++ src/pages/ContactUs/ContactUs.js
@@ -51,59 +51,61 @@ const ContactUs = () => {
 			<div className={styles.contact_us_body}>
 				<div className="container">
 					<div className={styles.contact_box}>
-						<div className="row">
-							<div className="col-md-6 d-none d-md-block">
+						<div className="row pi_12">
+							<div className="col-md-6 d-none d-md-block px-0">
 								<ContactInfo />
 							</div>
-							<div className={`col-md-6 ${styles.contact_form}`}>
-								<h2 className="fs_18 fw_700 primary pb_8 mb-4 bb_1_secondary">
-									Any Questions ? Just write us a message!
-								</h2>
-								<form onSubmit={formik.handleSubmit}>
-									<div className="row">
-										<div className="col-md-12 mb-3">
-											<InputText
-												name="name"
-												placeholder="Enter Your Name"
-												value={formik.values.name}
-												onChange={formik.handleChange}
-												className="input"
-											/>
+							<div className={`col-md-6 px-0 ${styles.contact_form}`}>
+								<div className="p2424">
+									<h2 className="fs_18 fw_700 primary pb_8 mb-4 bb_1_secondary">
+										Any Questions ? Just write us a message!
+									</h2>
+									<form onSubmit={formik.handleSubmit}>
+										<div className="row">
+											<div className="col-md-12 mb-3">
+												<InputText
+													name="name"
+													placeholder="Enter Your Name"
+													value={formik.values.name}
+													onChange={formik.handleChange}
+													className="input"
+												/>
+											</div>
+											<div className="col-md-6 mb-3">
+												<InputText
+													placeholder="Enter Your Email"
+													value={formik.values.email}
+													onChange={formik.handleChange}
+													className="input"
+												/>
+											</div>
+											<div className="col-md-6 mb-3">
+												<InputText
+													placeholder="Enter Your Phone Number"
+													value={formik.values.phone}
+													onChange={formik.handleChange}
+													className="input"
+												/>
+											</div>
+											<div className="col-md-12 mb-3">
+												<InputTextarea
+													placeholder="Enter Your Message"
+													value={formik.values.message}
+													onChange={formik.handleChange}
+													className="textArea"
+												/>
+											</div>
+											<div className="col-md-12">
+												<PrimaryBtn
+													btn
+													type="submit"
+													label="Send Message"
+													classes="w-100"
+												/>
+											</div>
 										</div>
-										<div className="col-md-6 mb-3">
-											<InputText
-												placeholder="Enter Your Email"
-												value={formik.values.email}
-												onChange={formik.handleChange}
-												className="input"
-											/>
-										</div>
-										<div className="col-md-6 mb-3">
-											<InputText
-												placeholder="Enter Your Phone Number"
-												value={formik.values.phone}
-												onChange={formik.handleChange}
-												className="input"
-											/>
-										</div>
-										<div className="col-md-12 mb-3">
-											<InputTextarea
-												placeholder="Enter Your Message"
-												value={formik.values.message}
-												onChange={formik.handleChange}
-												className="textArea"
-											/>
-										</div>
-										<div className="col-md-12">
-											<PrimaryBtn
-												btn
-												type="submit"
-												label="Send Message"
-												classes="w-100"
-											/>
-										</div>
-									</div>
-								</form>
+									</form>
+								</div>
 							</div>
 						</div>
 					</div>
